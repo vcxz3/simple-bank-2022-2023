7325029dc4782934ab0e76a1d5655f367eb32ebf
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-04-19 14:18:22 +0300

    Transactions class added

--- prog1-10-0810/main.py
+++ prog1-10-0810/main.py
@@ -28,6 +28,12 @@ class Customer:
         else:
             print(f"Konts ar numuru {number} nav atrasts.")
 
+# KOMANDAS UZDEVUMS 2
+# Izveido Account klases 2 metodes:
+# deposit (ielikt naudu kontā)
+# withdrawal (izneml naudu no konta)
+# padomā un piesaisti Transaction klasi ar Account klasi
+# izveido metodi balance Account klasei kas izvada konsolē konta bilanci
 
 class Account:
     def __init__(self, number, currency, balance, status = 'Active'):
@@ -36,20 +42,56 @@ class Account:
         self.balance = balance
         self.account_status = status
         self.datetime_created = datetime.datetime.now()
+        self.transactions = []
 
     def status(self):
         print(f"Account: {self.number} ({self.currency}) {self.balance} {self.account_status}")
 
     def show_balance(self):
         print(f"Account {self.number} has {self.balance} {self.currency}")
+
+    def deposit(self, amount, notes):
+        self.transactions.append(Transaction(amount, notes))
+        self.balance += amount
+
+    def withdraw(self, amount, notes):
+        self.transactions.append(Transaction(-amount, notes))
+        self.balance -= amount
+
+    def print_transactions(self):
+        for transaction in self.transactions:
+            transaction.status()
+
+    
+
+
+# KOMANDAS UZDEVUMS 1
+# Izveidot klasi Transaction (jeb darījumi)
+# __init__ metode (klases konstruktors)
+# status metode (izvadīs konsolē informāciju par konkrēto darījumu)
+
+
+class Transaction:
+    def __init__(self, amount, notes):
+        self.amount = amount
+        self.notes = notes
+        self.datetime_created = datetime.datetime.now()
+    
+    def status(self):
+        print(f"Transaction: {self.amount} {self.datetime_created} {self.notes}")
         
 
 customer1 = Customer("Anna", "Bērziņa", "78987-65448", "annab@someemail.co")
 customer1.add_account("123456789", "EUR", 200)
 customer1.add_account("987654321", "USD", 100)
+customer1.add_account("852145796", "JPY", 100)
 
-customer1.status()
-customer1.print_accounts()
-customer1.delete_account("852147963")
-customer1.print_accounts()
+customer1.accounts[0].deposit(600, "Alga par 03/2023")
+customer1.accounts[0].deposit(20, "Par dāvanu")
+customer1.accounts[0].withdraw(5, "Kafeinīca Gardumi")
+customer1.accounts[0].withdraw(120, "Keyboardz from online shop")
 
+customer1.accounts[1].deposit(200, "Freelance compensation 03/2023")
+
+for account in customer1.accounts:
+    account.print_transactions()
--- prog1-10-1200/main.py
+++ prog1-10-1200/main.py
@@ -28,6 +28,13 @@ class Customer:
             print(f"Account with number {account_number} not found.")
 
 
+# KOMANDAS UZDEVUMS 2
+# Izveido Account klases 2 metodes:
+# deposit (ielikt naudu kontā)
+# withdraw (izneml naudu no konta)
+# padomā un piesaisti Transaction klasi Account klasei
+# izveido metodi balance Account klasei kas izvada konsolē konta bilanci
+
 class Account:
     def __init__(self, number, currency, balance, account_status = 'Active'):
         self.number = number
@@ -35,9 +42,40 @@ class Account:
         self.balance = balance
         self.datetime_created = datetime.datetime.now()
         self.account_status = account_status
+        self.transactions = []
 
     def status(self):
         print(f"Account: {self.number} ({self.currency}) {self.balance}")
+
+    def deposit(self, amount, notes):
+        self.transactions.append(Transaction(amount, notes))
+        self.balance += amount
+
+    def withdraw(self, amount, notes):
+        self.transactions.append(Transaction(-amount, notes))
+        self.balance -= amount
+
+    def print_transactions(self):
+        self.status()
+        for transaction in self.transactions:
+            transaction.status()
+
+
+class Transaction:
+    def __init__(self, amount, notes):
+        self.amount = amount
+        self.notes = notes
+        self.datetime_created = datetime.datetime.now()
+    
+    def status(self):
+        print(f"Transaction:\t{self.amount}\t{self.datetime_created}\t{self.notes}")
+
+
+
+# KOMANDAS UZDEVUMS 1
+# Izveidot klasi Transaction (jeb darījumi)
+# __init__ metode (klases konstruktors)
+# status metode (izvadīs konsolē informāciju par konkrēto darījumu)
 
 
 customer1 = Customer("Anna", "Bērziņa", "123456-789456", "annab@someemail.com")
@@ -45,8 +83,17 @@ customer1.add_account("123456789", "EUR", 200)
 customer1.add_account("321654987", "USD", 100)
 customer1.add_account("8521236575", "JPY", 15000)
 
-customer1.status()
-customer1.print_accounts()
+customer1.accounts[0].deposit(600, "Alga par 03/2023")
+customer1.accounts[0].deposit(20, "par dāvanu")
+customer1.accounts[0].withdraw(5, "Pusdienās")
+customer1.accounts[0].withdraw(120, "Pirkums veikalā ABC")
 
-customer1.delete_account("321654988")
-customer1.print_accounts()
+customer1.accounts[1].deposit(50, "Freelance compensation")
+customer1.accounts[1].withdraw(90, "Amazon.com purchase")
+
+customer1.accounts[2].withdraw(2000, "Sushi restaurant 1")
+customer1.accounts[2].withdraw(3000, "Sushi restaurant 2")
+
+
+for account in customer1.accounts:
+    account.print_transactions()
